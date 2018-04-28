00e54a7fa74b9bf98d8f8ed8490f3c761227a2eb
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-04-28 17:15:29 +0200

    Essai de modif de la navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,18 +32,19 @@
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="#">Menu principal</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav justify-content-center">
+      <ul class="nav navbar-nav navbar-center">
         <li class="active"><a href="#">Accueil</a></li>
         <li><a href="#">Profil</a></li>
         <li><a href="#">Mon historique</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Créer un compte</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Se conencter</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
       </ul>
     </div>
   </div>
