0725908e6cef8f364206e75ebaf6b7a5145236da
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-04-28 18:08:00 +0200

    Modif du fil d'actu

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,18 +3,14 @@
 @section('content')
     <div class="container">
         <div class="row">
-          <div class="col-sm-3">
+          <div class="col-sm-2">
           </div>
-          <div class="col-sm-6">
-            <div class="row">
-              <div class="col">
-                <div class="card">
-                  <h3>Fil d'actualités</h3>
-                </div>
-              </div>
+          <div class="col-sm-8">
+            <div class="row" style="border: solid lightgrey 2px; padding: 10px 0px 10px 30px">
+              <h3>Fil d'actualités</h3>
             </div>
           </div>
-          <div class="col-sm-3">
+          <div class="col-sm-2">
           </div>
         </div>
     </div>
