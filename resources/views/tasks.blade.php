b8a5925e072c4814fa34d5f9b61215a011fecc87
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-05-15 15:57:22 +0200

    Modif

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -4,13 +4,15 @@
     <div class="container">
         <div class="row">
           <div class="sm-3">
+            &nbsp;
           </div>
           <div class="sm-6">
-            <div style="border: solid 2px; padding: 10px 0px 10px 30px">
+            <div style="border: solid lightgrey 2px; padding: 10px 0px 10px 30px">
               <h3>Fil d'actualités</h3>
             </div>
           </div>
           <div class="sm-3">
+            &nbsp;
           </div>
         </div>
         <div class="row">
