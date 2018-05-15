dacc36d5700151702efb65b753c5a2ea8864fda6
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-05-15 16:37:33 +0200

    Modif

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -33,5 +33,65 @@
           <div class="col-sm-2">
           </div>
         </div>
+        <div class="row" style="margin-top: 10px;">
+          <div class="col-sm-2">
+          </div>
+          <div class="col-sm-4">
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
+          </div>
+          <div class="col-sm-4">
+            <div class="jumbotron" style="padding: 25px">
+              <h3>Nom du bar</h1>
+              <p>Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>Ambiance, notation, etc...</p>
+              <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Voir la page du bar</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-2">
+          </div>
+        </div>
+        <div class="row" style="margin-top: 10px;">
+          <div class="col-sm-2">
+          </div>
+          <div class="col-sm-4">
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
+          </div>
+          <div class="col-sm-4">
+            <div class="jumbotron" style="padding: 25px">
+              <h3>Nom du bar</h1>
+              <p>Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>Ambiance, notation, etc...</p>
+              <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Voir la page du bar</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-2">
+          </div>
+        </div>
+        <div class="row" style="margin-top: 10px;">
+          <div class="col-sm-2">
+          </div>
+          <div class="col-sm-4">
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
+          </div>
+          <div class="col-sm-4">
+            <div class="jumbotron" style="padding: 25px">
+              <h3>Nom du bar</h1>
+              <p>Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>Ambiance, notation, etc...</p>
+              <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Voir la page du bar</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-2">
+          </div>
+        </div>
     </div>
 @endsection
