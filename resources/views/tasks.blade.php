fff5b3ae116737a387cf7bc49aa5f722962ed971
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-05-15 16:25:35 +0200

    Modif

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -13,13 +13,20 @@
           <div class="col-sm-2">
           </div>
         </div>
-        <div class="row">
+        <div class="row" style="margin-top: 10px;">
           <div class="col-sm-2">
           </div>
           <div class="col-sm-4">
-            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg"/>
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
           </div>
           <div class="col-sm-4">
+            <div class="jumbotron">
+              <h3>Nom du bar</h1>
+              <p class="lead">Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+              <button type="button" class="btn btn-primary">Voir la page</button>
+            </div>
           </div>
           <div class="col-sm-2">
           </div>
