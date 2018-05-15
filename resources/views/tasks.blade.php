a2b9375541a936e72528b27c50e03d8c41c179b8
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-05-15 16:03:48 +0200

    Modif

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,25 +3,25 @@
 @section('content')
     <div class="container">
         <div class="row">
-          <div class="col-2">
+          <div class="col-sm-2">
           </div>
-          <div class="col-8">
+          <div class="col-sm-8">
             <div style="border: solid lightgrey 2px; padding: 10px 0px 10px 30px">
               <h3>Fil d'actualités</h3>
             </div>
           </div>
-          <div class="col-2">
+          <div class="col-sm-2">
           </div>
         </div>
         <div class="row">
-          <div class="col-2">
+          <div class="col-sm-2">
           </div>
-          <div class="col-4">
+          <div class="col-sm-4">
             <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg"/>
           </div>
-          <div class="col-4">
+          <div class="col-sm-4">
           </div>
-          <div class="col-2">
+          <div class="col-sm-2">
           </div>
         </div>
     </div>
