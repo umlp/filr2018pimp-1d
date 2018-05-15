ba7b0a57e1cce4b13ba1be2914fd8a076f73f49f
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-05-15 15:51:49 +0200

    Modif

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,14 +3,14 @@
 @section('content')
     <div class="container">
         <div class="row">
-          <div class="col-sm-2">
+          <div class="sm-2">
           </div>
-          <div class="col-sm-8">
-            <div class="row" style="border: solid lightgrey 2px; padding: 10px 0px 10px 30px">
+          <div class="sm-8">
+            <div style="border: solid 2px; padding: 10px 0px 10px 30px">
               <h3>Fil d'actualités</h3>
             </div>
           </div>
-          <div class="col-sm-2">
+          <div class="sm-2">
           </div>
         </div>
     </div>
