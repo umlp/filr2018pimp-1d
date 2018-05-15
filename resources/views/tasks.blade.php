17b279c2766b3e26f5ab5b64ffcc58ec6553ca77
Arnaud QUILLENT <arnaud.quillent@gmail.com>
2018-05-15 16:57:08 +0200

    Modif

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -30,7 +30,9 @@
             </div>
           </div>
           <div class="col-sm-3">
-            <button type="button" class="btn btn-primary">Organiser son barathon</button>
+            <div class="hidden-sm-down">
+              <button type="button" class="btn btn-primary">Organiser son barathon</button>
+            </div>
           </div>
         </div>
         <div class="row" style="margin-top: 10px;">
@@ -51,10 +53,92 @@
             </div>
           </div>
           <div class="col-sm-3">
-            <span>
-              <input type="text" placeholder="Recherche">
-              <button type="button" class="btn btn-info">Rechercher</button>
-            </span>
+            <div class="hidden-sm-down">
+              <span>
+                <input type="text" placeholder="Recherche">
+                <button type="button" class="btn btn-info">Rechercher</button>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="row" style="margin-top: 10px;">
+          <div class="col-sm-3">
+          </div>
+          <div class="col-sm-3">
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
+          </div>
+          <div class="col-sm-3">
+            <div class="jumbotron" style="padding: 25px">
+              <h3>Nom du bar</h3>
+              <p>Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>Ambiance, notation, etc...</p>
+              <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Voir la page du bar</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+          </div>
+        </div>
+        <div class="row" style="margin-top: 10px;">
+          <div class="col-sm-3">
+          </div>
+          <div class="col-sm-3">
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
+          </div>
+          <div class="col-sm-3">
+            <div class="jumbotron" style="padding: 25px">
+              <h3>Nom du bar</h3>
+              <p>Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>Ambiance, notation, etc...</p>
+              <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Voir la page du bar</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+          </div>
+        </div>
+        <div class="row" style="margin-top: 10px;">
+          <div class="col-sm-3">
+          </div>
+          <div class="col-sm-3">
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
+          </div>
+          <div class="col-sm-3">
+            <div class="jumbotron" style="padding: 25px">
+              <h3>Nom du bar</h3>
+              <p>Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>Ambiance, notation, etc...</p>
+              <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Voir la page du bar</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+          </div>
+        </div>
+        <div class="row" style="margin-top: 10px;">
+          <div class="col-sm-3">
+          </div>
+          <div class="col-sm-3">
+            <img src="https://cuteoverload.files.wordpress.com/image7-967-14-_tphq.jpg" width="100%"/>
+          </div>
+          <div class="col-sm-3">
+            <div class="jumbotron" style="padding: 25px">
+              <h3>Nom du bar</h3>
+              <p>Description succinte du bar.</p>
+              <hr class="my-4">
+              <p>Ambiance, notation, etc...</p>
+              <p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Voir la page du bar</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
           </div>
         </div>
 
