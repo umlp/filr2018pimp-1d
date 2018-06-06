5505982dca3c46a2b5fa4eaa0ca989fce3e13b75
Arnaud Quillent <arnaud.quillent@insa-rouen.fr>
2018-06-06 18:08:21 +0200

    Form creerBar

--- resources/views/creerBar.blade.php
+++ resources/views/creerBar.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<form>
+    <fieldset>
+        <h4>Créer un bar</h4>
+        <br/>
+        <label for="nom">Nom du bar</label>
+        <input type="text" name="nom">
+        <label for="loc">Localisation</label>
+        <input type="text" name="loc">
+        <label for="desc">Description</label>
+        <input type="textarea" name="desc" rows="10">
+    </fieldset>
+</form>
 <input type="button" class="btn btn-primary" value="TEST">
 @endsection
