7b9ae2efb35c3259450fffb87afe02497e17ee75
Arnaud Quillent <arnaud.quillent@insa-rouen.fr>
2018-06-06 18:10:41 +0200

    Modif form

--- resources/views/creerBar.blade.php
+++ resources/views/creerBar.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
 <form>
-    <fieldset>
+    <div class="form-group">
         <h4>Créer un bar</h4>
         <br/>
         <label for="nom">Nom du bar</label>
-        <input type="text" name="nom">
+        <input class="form-control" type="text" name="nom">
         <label for="loc">Localisation</label>
-        <input type="text" name="loc">
+        <input class="form-control" type="text" name="loc">
         <label for="desc">Description</label>
         <input type="textarea" name="desc" rows="10">
-    </fieldset>
+    </div>
+    <button type="submit" class="btn btn-success">Créer</button>
 </form>
 <input type="button" class="btn btn-primary" value="TEST">
 @endsection
