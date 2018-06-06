51827f9618c079cd6fab5f131fe0e8038f3fe34f
Arnaud Quillent <arnaud.quillent@insa-rouen.fr>
2018-06-06 18:12:35 +0200

    Modif form*

--- resources/views/creerBar.blade.php
+++ resources/views/creerBar.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<form>
-    <div class="form-group">
-        <h4>Créer un bar</h4>
-        <br/>
-        <label for="nom">Nom du bar</label>
-        <input class="form-control" type="text" name="nom">
-        <label for="loc">Localisation</label>
-        <input class="form-control" type="text" name="loc">
-        <label for="desc">Description</label>
-        <input type="textarea" name="desc" rows="10">
-    </div>
-    <button type="submit" class="btn btn-success">Créer</button>
-</form>
-<input type="button" class="btn btn-primary" value="TEST">
+<div class="col-sm-3">
+</div>
+<div class="col-sm-6">
+    <form>
+        <div class="form-group">
+            <h4>Créer un bar</h4>
+            <br/>
+            <label for="nom">Nom du bar</label>
+            <input class="form-control" type="text" name="nom">
+            <label for="loc">Localisation</label>
+            <input class="form-control" type="text" name="loc">
+            <label for="desc">Description</label>
+            <input type="textarea" name="desc" rows="10">
+        </div>
+        <button type="submit" class="btn btn-success">Créer</button>
+    </form>
+</div>
+<div class="col-sm-3">
+</div>
 @endsection
