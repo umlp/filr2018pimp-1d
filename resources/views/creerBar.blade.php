e355a55f7399554c8f65cd5050c09d9f719558bd
Arnaud Quillent <arnaud.quillent@insa-rouen.fr>
2018-06-06 17:49:32 +0200

    Modif

--- resources/views/creerBar.blade.php
+++ resources/views/creerBar.blade.php
@@ -1,3 +1,3 @@
 @extends('layouts.app')
 
-@section('content')
+<input type="button" class="btn btn-primary" value="TEST">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,54 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Get a Beer !</title>
-
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-
-    <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
-    <style>
-        body {
-            font-family: 'Lato';
-        }
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
-</head>
-<body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Menu principal</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-center">
-                    <li class="active"><a href="#">Accueil</a></li>
-                    <li><a href="#">Profil</a></li>
-                    <li><a href="#">Mon historique</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Créer un compte</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+@extends('layouts.app')
     <div class="row">
         <div class="col-sm-3">
         </div>
@@ -189,5 +139,3 @@
         <div class="col-sm-3">
         </div>
     </div>
-</body>
-</html>
